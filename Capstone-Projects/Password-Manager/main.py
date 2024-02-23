670be037ab74ebc79b0691e65b5a4cae612c3725
Debaditya Bhar <bhar.debaditya@gmail.com>
2024-02-23 18:17:23 +0530

    Day 30 End

--- Capstone-Projects/Password-Manager/main.py
+++ Capstone-Projects/Password-Manager/main.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 # Constants.
 FONT = ('Arial', 16, 'normal')
-PASSWORD_FILE = 'data.txt'
+PASSWORD_FILE = 'data.json'
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -48,7 +49,53 @@ def generate_random_password():
     window.clipboard_append(random_password)
 
 
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+    
+def search_data():
+    """Search the existing credentials from the file."""
+    # Get the input entry data.
+    website = website_entry.get()
+
+    try:
+        with open(PASSWORD_FILE, 'r') as data_file:
+            # Get the existing data from the password file.
+            data = json.load(data_file) 
+    except FileNotFoundError:
+        messagebox.showerror(title='Error', message='No Password File Exists!')
+    else:
+        try:
+            website_data = data[website]
+        except KeyError:
+            messagebox.showerror(title='Error', message=f'No credentials exists for the {website}')
+        else:
+            messagebox.showinfo(title=website, message=f'Website: {website}\nEmail: {website_data['id']}\nPassword: {website_data['password']}')
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+    
+def write_json_data(json_data):
+    """Writes JSON data into the password file."""
+    data = None
+
+    try:
+        with open(PASSWORD_FILE, 'r') as data_file:
+            # Get the existing data from the password file.
+            data = json.load(data_file)            
+
+    except:
+        data = json_data
+
+    else:
+        # Get the updated data.
+        data.update(json_data)
+
+    finally:
+        # Write the data into password file.
+        with open(PASSWORD_FILE, 'w') as data_file:
+            json.dump(data, data_file, indent=4)
+
+
 def save_password_data():
     """Stores the input data into the file."""
 
@@ -63,20 +110,30 @@ def save_password_data():
         return
 
 
-    is_ok = messagebox.askokcancel(title=website, 
-            message=f'The details enterd as follows:\nEmail: {id}\nPassword: {password}\nIs it okay to save?')
+    # is_ok = messagebox.askokcancel(title=website, 
+    #         message=f'The details enterd as follows:\nEmail: {id}\nPassword: {password}\nIs it okay to save?')
     
-    if is_ok:
-        # Format the data row to be written.
-        data_row = f'{website} | {id} | {password}\n'
+    # if is_ok:
+    #     # Format the data row to be written.
+    #     data_row = f'{website} | {id} | {password}\n'
 
-        # Write the data row into the password file.
-        with open(PASSWORD_FILE, 'a') as file:
-            file.write(data_row)
+    #     # Write the data row into the password file.
+    #     with open(PASSWORD_FILE, 'a') as file:
+    #         file.write(data_row)
+
+    # Format the data to be written in JSON.
+    new_data = {
+        website: {
+            'id': id,
+            'password': password 
+        }
+    }
+    
+    write_json_data(new_data)
         
-            # Clear the entry widgets.
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+    # Clear the entry widgets.
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
     
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -91,12 +148,12 @@ logo_canvas = Canvas(window, width=200, height=200, highlightthickness=0)
 logo_canvas.create_image(100, 100, image=logo)
 
 # Label widgets.
-website_label = Label(window, text='Website:', font=FONT, pady=5)
-id_label = Label(window, text='Email/Username:', font=FONT, pady=5)
-password_label = Label(window, text='Password:', font=FONT, pady=5)
+website_label = Label(window, text='Website:', font=FONT)
+id_label = Label(window, text='Email/Username:', font=FONT)
+password_label = Label(window, text='Password:', font=FONT)
 
 # Entry widgets.
-website_entry = Entry(window, font=FONT, width=35)
+website_entry = Entry(window, font=FONT, width=21)
 website_entry.focus()
 
 id_entry = Entry(window, font=FONT, width=35)
@@ -107,16 +164,18 @@ password_entry = Entry(window, font=FONT, width=21)
 # Button widgets.
 gen_pass_btn = Button(window, text='Generate Password', font=FONT, command=generate_random_password)
 add_btn = Button(window, text='Add', font=FONT, width=36, command=save_password_data)
+search_btn = Button(window, text='Search', font=FONT, command=search_data, width=15)
 
 # Geometry Manager Methods
 logo_canvas.grid(row=0, column=1)
-website_label.grid(row=1, column=0, sticky='NE')
-website_entry.grid(row=1, column=1, columnspan=2, pady=5)
-id_label.grid(row=2, column=0, sticky='NE')
-id_entry.grid(row=2, column=1, columnspan=2, pady=5)
-password_label.grid(row=3, column=0, sticky='NE')
-password_entry.grid(row=3, column=1, pady=5, padx=5, sticky='NE')
-gen_pass_btn.grid(row=3, column=2, pady=5, sticky='NE')
-add_btn.grid(row=4, column=1, columnspan=2, pady=5)
+website_label.grid(row=1, column=0)
+website_entry.grid(row=1, column=1)
+search_btn.grid(row=1, column=2)
+id_label.grid(row=2, column=0)
+id_entry.grid(row=2, column=1, columnspan=2)
+password_label.grid(row=3, column=0)
+password_entry.grid(row=3, column=1)
+gen_pass_btn.grid(row=3, column=2)
+add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
